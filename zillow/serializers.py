228f2e0c6081724e132d6ebffc99c06ca9cd9556
aziza <aziza.abduraufova7@gmail.com>
2025-08-21 14:39:30 +0500

    create project, models, serializers

---
+++ zillow/serializers.py
@@ -0,0 +1,14 @@
+from zillow.models import Property, PropertyType
+from rest_framework import serializers
+
+class PropertyTypeSerializer(serializers.ModelSerializer):
+    properties = PropertySerializer(many=True, read_only=True)
+    class Meta:
+        model = PropertyType
+        fields = '__all__'
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = '__all__'
+        read_only_fields = 'created_at'
