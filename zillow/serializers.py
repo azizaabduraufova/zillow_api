75c6cc1d911be17321d8f5d981d266b6071f833c
Mila <makhliyoabboskhonova@gmail.com>
2025-08-22 18:44:03 +0500

    CRUD done

--- zillow/serializers.py
+++ zillow/serializers.py
@@ -6,7 +6,7 @@ class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
         fields = '__all__'
-        read_only_fields = 'created_at'
+        read_only_fields = ['created_at']
 
 class PropertyTypeSerializer(serializers.ModelSerializer):
     properties = PropertySerializer(many=True, read_only=True)
--- zillow/views.py
+++ zillow/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from zillow.models import PropertyType, Property
+from zillow.serializers import PropertyTypeSerializer,PropertySerializer
+
 
 # Create your views here.
+
+
+class PropertyTypeGet(generics.ListCreateAPIView):
+    queryset = PropertyType.objects.all()
+    serializer_class = PropertyTypeSerializer
+
+
+class PropertyTypeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = PropertyType.objects.all()
+    serializer_class = PropertyTypeSerializer
+
+
+class PropertyGet(generics.ListCreateAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
+
+class PropertyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
