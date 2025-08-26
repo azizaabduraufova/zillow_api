a6b96c1108e34a60471f699b3838962cefe572a1
aziza <aziza.abduraufova7@gmail.com>
2025-08-26 15:48:17 +0500

    permissions

--- zillow/views.py
+++ zillow/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics, filters
 
 from zillow.models import PropertyType, Property, Profile
-from zillow.serializers import PropertyTypeSerializer,PropertySerializer, UserProfileSerializer
+from zillow.permissions import IsOwnerOrReadOnly, IsSellerOrReadOnly
+from zillow.serializers import PropertyTypeSerializer, PropertySerializer, UserProfileSerializer
 from rest_framework import generics, filters
-
 
 
 class PropertyTypeGet(generics.ListCreateAPIView):
@@ -14,13 +14,9 @@ class PropertyTypeGet(generics.ListCreateAPIView):
     search_fields = ['name']
 
 
-
-
-
 class PropertyTypeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = PropertyType.objects.all()
     serializer_class = PropertyTypeSerializer
-
 
 
 class PropertyGet(generics.ListCreateAPIView):
@@ -28,16 +24,19 @@ class PropertyGet(generics.ListCreateAPIView):
     serializer_class = PropertySerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['title']
-    ordering_fields=['created_at','price']
-    ordering=['-created_at']
+    ordering_fields = ['created_at', 'price']
+    ordering = ['-created_at']
+    permission_classes = [IsSellerOrReadOnly]
 
-
-
+    def perform_create(self, serializer):
+        serializer.save(listed_by=self.request.user)
 
 
 class PropertyDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+    permission_classes = [IsSellerOrReadOnly]
+
 
 class UserProfileGet(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
@@ -45,6 +44,11 @@ class UserProfileGet(generics.ListCreateAPIView):
     filter_backends = [filters.SearchFilter]
     search_fields = ['user__username']
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = UserProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
